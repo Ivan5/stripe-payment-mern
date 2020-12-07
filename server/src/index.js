c69466259048552d7057b7c1e333c4f0dc151548
Ivan5 <converse_ivan@hotmail.com>
2020-12-07 14:16:59 -0600

    Add some dependencies and use in the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,34 @@
 const express = require("express");
+const session = require("express-session");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Registering Session Middleware
+app.use(
+  session({
+    cookie: {
+      maxAge: 3600000 * 24,
+    },
+    saveUninitialized: false,
+    resave: false,
+    secret: "basjhdbasjhasdbasjdbasduh",
+  })
+);
+
+app.get("/", (req, res) => {
+  if (req.session.authenticated) {
+    res.send({ status: 200, session: req.session, id: req.sessionID });
+  } else {
+    req.session.authenticated = {
+      username: "hello",
+    };
+    res.send({ status: 200, session: req.session, id: req.sessionID });
+  }
+  res.send(200);
+});
+
+app.listen(PORT, () => console.log(`Listening to request on port ${PORT}`));
